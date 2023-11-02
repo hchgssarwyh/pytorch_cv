6129877ad33c331b88910dc0aa0eb76339b64647
hchgssarwyh <63221096+hchgssarwyh@users.noreply.github.com>
2023-11-03 01:17:15 +0300

    Add files via upload

---
+++ cv_module/data.py
@@ -0,0 +1,44 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import cv_module
+
+
+transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+def trainloader():
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+    return trainloader
+
+def testloader():
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2) 
+    return testloader
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def show_random_images(loader):
+    dataiter = iter(trainloader)
+    images, labels = next(dataiter)
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join(f'{cv_module.classes[labels[j]]:5s}' for j in range(batch_size)))
---
+++ testing module.py
@@ -0,0 +1,15 @@
+import cv_module.data
+import cv_module.neural_network
+import cv_module.train
+import cv_module
+
+net = cv_module.neural_network.Net()
+trainloader = cv_module.data.trainloader()
+
+cv_module.train.train_nn(net, trainloader)
+
+testloader = cv_module.data.trainloader()
+
+cv_module.neural_network.test_Net(net, testloader)
+
+cv_module.neural_network.accuracy_for_classes(net, testloader)
