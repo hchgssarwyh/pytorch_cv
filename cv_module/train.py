11d0ac1255e50877f64e07df0d9b0bb9b50598d3
hchgssarwyh <63221096+hchgssarwyh@users.noreply.github.com>
2023-11-10 16:42:28 +0300

    Update train.py

--- cv_module/train.py
+++ cv_module/train.py
@@ -1,40 +1,8 @@
-import torch.optim as optim
-import torch.nn as nn
+import pytorch_lightning as L
+from pytorch_lightning.loggers import WandbLogger
+import cv_module
 import wandb
-import cv_module.neural_network
 
-run = wandb.init(
-    project="pytorch_classification",
-    notes="My first experiment",
-)
-
-wandb.config = {"epochs": 4, "learning_rate": 0.0005, "batch_size": 4}
-def train_nn(net, trainset):
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.0005, momentum=0.9)
-
-    for epoch in range(4):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainset, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                wandb.log({'loss': running_loss/2000})
-                running_loss = 0.0
-    cv_module.neural_network.save_Net(net, 'model')            
-    print('Finished Training')
+wandb_logger = WandbLogger(log_model="all")
+dm = cv_module.data.CIFAR10DataModule(data_dir="dataset/", batch_size=cv_module.batch_size, num_workers=4)
+trainer = L.Trainer(logger = wandb_logger, accelerator = "auto", devices = 1, min_epochs = 1, max_epochs = cv_module.epochs)
