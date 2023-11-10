2b6d6c4c122b53bbd82235bb4297ea2b2c0315ad
hchgssarwyh <63221096+hchgssarwyh@users.noreply.github.com>
2023-11-10 16:42:12 +0300

    Update neural_network.py

--- cv_module/neural_network.py
+++ cv_module/neural_network.py
@@ -1,45 +1,79 @@
+from typing import Any
+from pytorch_lightning.utilities.types import STEP_OUTPUT, OptimizerLRScheduler
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 import torch
 import cv_module
+import pytorch_lightning as L
+import torchmetrics
 
-class Net(nn.Module):
-    def __init__(self):
+ 
+class Net(L.LightningModule):
+    def __init__(self, num_classes):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128,kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.dropout = nn.Dropout(0.2)
+
+
+        self.fc1 = nn.Linear(256*4*4, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 10)
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.accuracy = torchmetrics.Accuracy(task = 'multiclass', num_classes = num_classes)
+        self.f1_score = torchmetrics.F1Score(task = 'multiclass', num_classes = num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.dropout(self.conv1(x)))
+        x = self.pool(F.relu(self.dropout(self.conv2(x))))
+
+        x = F.relu(self.dropout(self.conv3(x)))
+        x = self.pool(F.relu(self.dropout(self.conv4(x))))
+
+        x = F.relu(self.dropout(self.conv5(x)))
+        x = self.pool(F.relu(self.dropout(self.conv6(x))))
+
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.dropout(self.fc1(x)))
+        x = F.relu(self.dropout(self.fc2(x)))
+        x = F.log_softmax(self.dropout(self.fc3(x)))
         return x
-    
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        scores = self(x)
+        loss = self.loss_fn(scores, y)
+        accuracy = self.accuracy(scores, y)
+        f1_score = self.f1_score(scores, y)
+        self.log_dict({'train_loss':loss, 'train_accuracy':accuracy, 'train_f1_score':f1_score})
+        return loss
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        scores = self(x)
+        loss = self.loss_fn(scores, y)
+        self.log('val_loss',loss)
+        return loss
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        scores = self(x)
+        loss = self.loss_fn(scores, y)
+        accuracy = self.accuracy(scores, y)
+        self.log('test_accuracy', accuracy)
+        return loss
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr = cv_module.learning_rate) 
+        
+        
 def save_Net(net, PATH):
     torch.save(net.state_dict(), PATH)
 
-def test_Net(net, testloader):
-    correct = 0
-    total = 0
-# since we're not training, we don't need to calculate the gradients for our outputs
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            # calculate outputs by running images through the network
-            outputs = net(images)
-            # the class with the highest energy is what we choose as prediction
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
 
-    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 
 def accuracy_for_classes(net, testloader):
     # prepare to count predictions for each class
