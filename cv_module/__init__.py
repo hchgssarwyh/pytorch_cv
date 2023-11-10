13746e36b68ac7858d3c7fd7da14ac4a051143a4
hchgssarwyh <63221096+hchgssarwyh@users.noreply.github.com>
2023-11-10 16:43:19 +0300

    Update __init__.py

--- cv_module/__init__.py
+++ cv_module/__init__.py
@@ -1,2 +1,13 @@
+import wandb
+import torch
+
+batch_size = 64
+epochs = 15
+learning_rate = 2e-4
+
+
 classes = ('plane', 'car', 'bird', 'cat',
         'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+random_seed = 42
+torch.manual_seed(random_seed);
